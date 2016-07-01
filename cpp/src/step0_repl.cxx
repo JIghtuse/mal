cc54111519f9db16e1da26f39b02d5fec6079359
Boris Egorov <egorov@linux.com>
2016-07-02 00:32:18 +0600

    cpp: Complete step0 (REPL)

--- cpp/src/step0_repl.cxx
+++ cpp/src/step0_repl.cxx
@@ -1,3 +1,39 @@
+#include <iostream>
+#include <string>
+
+namespace {
+    const char* prompt = "user> ";
+} // namespace
+
+const std::string read(const std::string& in)
+{
+    return in;
+}
+
+const std::string eval(const std::string& in)
+{
+    return in;
+}
+
+const std::string print(const std::string& in)
+{
+    return in;
+}
+
+const std::string rep(const std::string& in)
+{
+    return print(eval(read(in)));
+}
+
 int main()
 {
+    while (true) {
+        if (std::cin.eof()) {
+            break;
+        }
+        std::cout << prompt;
+        std::string s;
+        std::getline(std::cin, s);
+        std::cout << rep(s) << std::endl;
+    }
 }
